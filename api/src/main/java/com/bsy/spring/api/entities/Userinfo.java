0d8ec625b37f622f0e3ddb95668ef94a4d75a496
baosiyou <baosiyou@xiongying.com>
2021-01-12 18:30:02 +0800

    update

--- api/src/main/java/com/bsy/spring/api/entities/Userinfo.java
+++ api/src/main/java/com/bsy/spring/api/entities/Userinfo.java
@@ -2,13 +2,15 @@ package com.bsy.spring.api.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Userinfo {
-    private int userid;
+    private Integer userid;
     private String username;
     private Date createtime;
 }
